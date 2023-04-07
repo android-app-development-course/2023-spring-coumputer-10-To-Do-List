1ccf2f8e232b7f6e90a7a7391e5f7d82aabed6d6
Jerome-2046 <2046769428@qq.com>
2023-04-07 10:33:03 +0800

    complete ui with date_time_picker dialog

--- app/src/main/java/com/example/myapplication2/MainActivity2.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity2.kt
@@ -5,11 +5,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import com.loper7.date_time_picker.controller.DateTimeController
+import com.loper7.date_time_picker.dialog.CardDatePickerDialog
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        CardDatePickerDialog.builder(this)
+            .setTitle("SET MAX DATE")
+            .setOnChoose {millisecond->
+
+            }.build().show()
+
+
+
+
+
 
         //
         var data = intent.getStringExtra("extra_data")
